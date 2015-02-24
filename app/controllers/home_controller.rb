aca216b942c9aedc950aae669e772d39cecefc09
STUDENT <suzanna5000@gmail.com>
2015-02-24 15:05:00 +0530

    My first blogapp created

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def home
+  end
+  def about
+  end
+  def contact_us
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "home_suz"=>"home#home"
+  get "about"=>"home#about"
+  get "contact_us"=>"home#contact_us"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
